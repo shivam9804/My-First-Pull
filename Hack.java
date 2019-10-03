c9469e7e12485dd16d0bb13713b0be70306c5ccd
Digital Takeoff <53189040+digitaltakeoff20@users.noreply.github.com>
2019-10-03 09:33:13 +0530

    Add files via upload

---
+++ Hack.java
@@ -0,0 +1,10 @@
+Public Class Hack{
+    void testFunc(int test){
+        int f = test;
+        for(i = 0; i < 5; i++){
+            // Print value of f by adding value of i on each iteration
+            system.ot.print("f+i");
+        }
+    }
+}
+
