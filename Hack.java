2ee1d3fdaac5158c47ba523cd9cb47e90dfd88d8
Shivam Singh <32814815+shivam9804@users.noreply.github.com>
2019-10-03 19:40:48 +0530

    Update Hack.java

--- Hack.java
+++ Hack.java
@@ -1,12 +1,13 @@
 Public Class Hack{
     void testFunc(int test){
         int f = test;
-        for(i = 0; i < 5; i++){
+        for(int i = 0; i < 5; i++){
             // Print value of f by adding value of i on each iteration
             System.out.print(f+i);
         }
     }
-    int main(){
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
         int n = sc.nextInt();
         testFun(n);
